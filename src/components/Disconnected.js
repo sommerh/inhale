ebfd3df844089089d85588a3f8a75d15a00b4849
Hannah Sommer <hannah.j.sommer@gmail.com>
2022-05-01 15:38:37 -0400

    Add alt prop to img tags on disconnected

--- src/components/Disconnected.js
+++ src/components/Disconnected.js
@@ -6,27 +6,27 @@ const Disconnected = () => {
     <div className="ball-container">
       <div className="disconnected">
         <div className="sense">
-          <img src="eye.jpeg" />
+          <img src="eye.jpeg" alt="eye" />
           <p className="number"> 5 </p>
           <p> things you can see</p>
         </div>
         <div className="sense">
-          <img src="hand.jpeg" />
+          <img src="hand.jpeg" alt="hand" />
           <p className="number"> 4 </p>
           <p>things you can feel</p>
         </div>
         <div className="sense">
-          <img src="ear.jpeg" />
+          <img src="ear.jpeg" alt="ear" />
           <p className="number"> 3 </p>
           <p>things you can hear</p>
         </div>
         <div className="sense">
-          <img src="nose.png" />
+          <img src="nose.png" alt="nose" />
           <p className="number"> 2 </p>
           <p>things you can smell/taste</p>
         </div>
         <div className="sense">
-          <img src="heart2.jpeg" />
+          <img src="heart2.jpeg" alt="heart" />
           <p className="number"> 1 </p>
           <p>thing you like about yourself</p>
         </div>
